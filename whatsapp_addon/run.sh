50cb21220ebca74e09537ae95fead82f76a2e2ea
ronmichel <40127030+ronmichel@users.noreply.github.com>
2024-01-31 10:08:04 +0100

    Update run.sh

--- whatsapp_addon/run.sh
+++ whatsapp_addon/run.sh
@@ -3,8 +3,8 @@ set +u
 
 sed -i "s/{{HOSTNAME}}/$HOSTNAME/g" custom_component/whatsapp.py
 
-mkdir -p config/custom_components/whatsapp
-cp --recursive /custom_component/* config/custom_components/whatsapp/
+mkdir -p config/custom_components/whatsapp_self
+cp --recursive /custom_component/* config/custom_components/whatsapp_self/
 bashio::log.info "Installed custom component."
 
 node index
