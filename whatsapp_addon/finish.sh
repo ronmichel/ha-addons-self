7197bb70d93f8157e01c4f6e3f14eae44da88f06
ronmichel <40127030+ronmichel@users.noreply.github.com>
2024-01-31 10:06:56 +0100

    Update finish.sh

--- whatsapp_addon/finish.sh
+++ whatsapp_addon/finish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bashio
 
-rm -rf config/custom_components/whatsapp
+rm -rf config/custom_components/whatsapp_self
 
 if [ -z "$(ls -A config/custom_components)" ]; then
    rm -rf config/custom_components
